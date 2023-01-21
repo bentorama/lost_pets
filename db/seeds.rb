611df069adf298a17a008cbe18fc8f6351ee10eb
Edward Bent <bentorama@gmail.com>
2023-01-21 17:14:29 +0000

    read actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+i = 0
+10.times do
+  i += 1
+  puts "Creating pet number #{i}"
+  Pet.create(
+    name: Faker::Creature::Dog.name,
+    address: Faker::Address.street_name,
+    species: %w[dog cat chicken rabbit ferret tortoise].sample,
+    found_on: Faker::Date.between(from: '2020-01-01', to: Date.today)
+  )
+end
